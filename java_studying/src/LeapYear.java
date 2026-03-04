9274d14c0fff54dc4b53d990d8c248fc023dd836
santana <glebklyga2775@gmail.com>
2026-03-04 16:44:50 +0200

    Java_Homeworks

---
+++ java_studying/src/LeapYear.java
@@ -0,0 +1,14 @@
+public class LeapYear  {
+    boolean isLeapYear(int year) {
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        LeapYear checker = new LeapYear();
+        System.out.println(checker.isLeapYear(2032));
+    }
+}
+
---
+++ java_studying/src/Operator_precendence.java
@@ -0,0 +1,14 @@
+public class Operator_precendence {
+    public static void main(String[] args) {
+        double a = 20;
+        double b = 80;
+        double result = (a + b) * 100;
+        double reminder = result % 40;
+        boolean isNoReminder = (reminder == 0);
+        System.out.println(isNoReminder);
+        if (!isNoReminder) {
+            System.out.println("got some remainder");
+        }
+    }
+}
+
