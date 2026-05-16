c9a0ef99e90b4bc04664da9bcaa24fcca7cb3e39
santana <glebklyga2775@gmail.com>
2026-05-16 16:19:33 +0300

    update

--- java_studying/src/Challenge1_Operators/IdCode.java
+++ java_studying/src/Challenge1_Operators/IdCode.java
@@ -1,7 +1,18 @@
 package Challenge1_Operators;
 public class IdCode {
     public static void main(String[] args) {
+        double a = 123.00;
+        double b = 77.00;
 
+        double result = (a + b) * 100.00;
+        double remainder = result % 40.00;
+
+        boolean isZero = remainder == 0.00;
+
+        System.out.println(isZero);
+
+        if (!isZero) {
+            System.out.println("got some remainder");
+        }
     }
-
 }
